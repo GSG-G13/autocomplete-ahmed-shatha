e64b539895b8b9a302e8bf1ecad7fee7e094f61a
shatha <shathaamin63@gmail.com>
2023-03-21 20:02:00 +0200

    edit fetch method Closes #7

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -3,7 +3,7 @@ const fetch = (url, cb) => {
     xhr.onreadystatechange = () => {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
-          let data = JSON.parse(xhr.responseText);
+         const data = JSON.parse(xhr.responseText);
           cb(data);
         } else {
           cb(null);
